e1c99dd1aa2b7772d4ca9014081dddd54951a680
Utkarsh Kukreti <utkarshkukreti@gmail.com>
2018-09-01 08:45:27 +0530

    feat: AST

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,68 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+pub enum Stmt {
+    Block(Vec<Stmt>),
+    Empty,
+    Expr(Expr),
+    If(Expr, Box<Stmt>, Option<Box<Stmt>>),
+    Return(Option<Expr>),
+    Var(String, Option<Expr>),
+    While(Expr, Box<Stmt>),
+}
+
+pub enum Expr {
+    Array(Vec<Expr>),
+    Assign(String, Box<Expr>),
+    Binary(BinaryOperator, Box<Expr>, Box<Expr>),
+    Bool(bool),
+    Call(Box<Expr>, Vec<Expr>),
+    Function(Function),
+    Member(Box<Expr>, Box<Expr>),
+    Null,
+    Number(f64),
+    Object(Vec<(String, Expr)>),
+    String(String),
+    This,
+    Unary(UnaryOperator, Box<Expr>),
+    Undefined,
+    Var(String),
+}
+
+pub struct Function {
+    pub params: Vec<String>,
+    pub body: Vec<Stmt>,
+}
+
+pub enum UnaryOperator {
+    Add,
+    BitwiseNot,
+    Delete,
+    Not,
+    Sub,
+    Typeof,
+    Void,
+}
+
+pub enum BinaryOperator {
+    Add,
+    And,
+    BitwiseAnd,
+    BitwiseOr,
+    BitwiseXor,
+    Div,
+    Eq,
+    Eqq,
+    Gt,
+    Gte,
+    In,
+    InstanceOf,
+    Lt,
+    Lte,
+    Mod,
+    Mul,
+    NotEq,
+    NotEqq,
+    Or,
+    Shl,
+    Shr,
+    Sub,
+    UnsignedShr,
 }
